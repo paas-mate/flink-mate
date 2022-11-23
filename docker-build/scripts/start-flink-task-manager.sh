89c9b56c120077858608e9299a4a76637c8d3a38
xiang092689 <58943501+xiang092689@users.noreply.github.com>
2022-11-23 17:49:50 +0800

    flink-job-manager config (#1)

--- docker-build/scripts/start-flink-task-manager.sh
+++ docker-build/scripts/start-flink-task-manager.sh
@@ -2,6 +2,6 @@
 
 echo "" >> $FLINK_HOME/conf/flink-conf.yaml
 
-echo "jobmanager.rpc.address: flink-master" >> $FLINK_HOME/conf/flink-conf.yaml
+echo "jobmanager.rpc.address: flink-job-manager" >> $FLINK_HOME/conf/flink-conf.yaml
 
 bash $FLINK_HOME/bin/taskmanager.sh start
