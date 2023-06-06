f2696cf11a5c378aa2fb054738d3344d1fe08ef1
goflutterjava <goflutterjava@gmail.com>
2023-06-06 20:09:33 +0800

    job manager and task manager share config

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+bash -x $FLINK_HOME/mate/scripts/gen_flink_conf.sh
 if [ "$CLUSTER_ROLE" == "JOB_MANAGER" ]; then
     bash -x $FLINK_HOME/mate/scripts/start-flink-job-manager.sh
 elif [  "$CLUSTER_ROLE" == "TASK_MANAGER" ]; then
--- docker-build/scripts/start-flink-task-manager.sh
+++ docker-build/scripts/start-flink-task-manager.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-echo "" > $FLINK_HOME/conf/flink-conf.yaml
-
-echo "jobmanager.rpc.address: flink-job-manager" >> $FLINK_HOME/conf/flink-conf.yaml
-
-echo "taskmanager.bind-host: 0.0.0.0" >> $FLINK_HOME/conf/flink-conf.yaml
-echo "taskmanager.memory.process.size: 4096mb" >> $FLINK_HOME/conf/flink-conf.yaml
-
 bash $FLINK_HOME/bin/taskmanager.sh start
